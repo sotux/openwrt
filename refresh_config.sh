b0e021e3ac401a78422c3b9c7f7ea6d63bdfc927
Zheng Qian <sotux82@gmail.com>
2019-08-14 10:40:30 +0800

    build: add a script to build my custom roms
    Signed-off-by: James Qian <sotux82@gmail.com>

---
+++ refresh_config.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+refresh_conf()
+{
+	echo "refresh config for $1"
+	cd env
+	git checkout "$1"
+	git fetch origin
+	git reset --hard "origin/$1"
+	cd -
+	make menuconfig
+	cd env
+	git commit --amend --no-edit -a
+	git push --force-with-lease
+	cd -
+}
+
+refresh_conf "ac58u-19.07"
+refresh_conf "newifi3-19.07"
+refresh_conf "tl-wr841nv10-ath79-19.07"
+refresh_conf "tl-wr841nv7-ath79-19.07"
+refresh_conf "tl-wr841nv8-ath79-19.07"
+refresh_conf "wndr3700v4-ar71xx-19.07"
+refresh_conf "wndr3800-ath79-19.07"
+refresh_conf "wndr4300v1-ar71xx-19.07"
+refresh_conf "wr1200js-19.07"
