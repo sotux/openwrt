5cde7add5a7f4b159e792b47f919933fa83c3b42
James Qian <sotux82@gmail.com>
2019-11-13 09:37:19 +0800

    autobuild: detect cpu processors for make job
    Signed-off-by: James Qian <sotux82@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-./scripts/env switch ac58u-19.07 && make -j8
+CPU_PROCESSORS=`cat /proc/cpuinfo | grep "processor" | wc -l`
 
-./scripts/env switch newifi3-19.07 && make -j8
-./scripts/env switch wr1200js-19.07 && make -j8
+echo "This computer has $CPU_PROCESSORS processor(s)."
 
-./scripts/env switch tl-wr841nv10-ath79-19.07 && make -j8
-./scripts/env switch tl-wr841nv7-ath79-19.07 && make -j8
-./scripts/env switch tl-wr841nv8-ath79-19.07 && make -j8
-./scripts/env switch wndr3800-ath79-19.07 && make -j8
 
-./scripts/env switch wndr3700v4-ar71xx-19.07 && make -j8
-./scripts/env switch wndr4300v1-ar71xx-19.07 && make -j8
+./scripts/env switch ac58u-19.07 && make -j$CPU_PROCESSORS
+
+./scripts/env switch newifi3-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch wr1200js-19.07 && make -j$CPU_PROCESSORS
+
+./scripts/env switch tl-wr841nv10-ath79-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch tl-wr841nv7-ath79-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch tl-wr841nv8-ath79-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr3800-ath79-19.07 && make -j$CPU_PROCESSORS
+
+./scripts/env switch wndr3700v4-ar71xx-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr4300v1-ar71xx-19.07 && make -j$CPU_PROCESSORS
 
