9e7fb5b70196651a401d8b3ba67c9bd91decdfb3
Zheng Qian <sotux82@gmail.com>
2021-07-04 07:37:55 +0800

    ath79: add Netgear WNR2000 v3 support
    Netgear WNR2000 v3:
     - cpu Atheros AR7241 @360MHz
     - flash 4MB
     - ram 32MB
     - ethernet 10/100: 1xWan + 4xLan
     - radio AR9287
    
    Signed-off-by: Zheng Qian <sotux82@gmail.com>

--- target/linux/ath79/image/tiny-netgear.mk
+++ target/linux/ath79/image/tiny-netgear.mk
@@ -9,6 +9,23 @@ define Device/netgear_ar7240
   $(Device/netgear_ath79)
 endef
 
+define Device/netgear_wnr2000-v3
+  ATH_SOC := ar7241
+  DEVICE_TITLE := Netgear WNR2000v3
+  DEVICE_DTS := ar7241_netgear_wnr2000-v3
+  NETGEAR_KERNEL_MAGIC := 0x32303033
+  KERNEL := kernel-bin | append-dtb | lzma -d20 | netgear-uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma -d20 | netgear-uImage lzma
+  NETGEAR_BOARD_ID := WNR2000V3
+  NETGEAR_HW_ID := 29763551+04+32
+  IMAGE_SIZE := 3712k
+  IMAGES := sysupgrade.bin factory.img
+  IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | netgear-squashfs | append-rootfs | pad-rootfs
+  IMAGE/sysupgrade.bin := $$(IMAGE/default) | append-metadata | check-size $$$$(IMAGE_SIZE)
+  IMAGE/factory.img := $$(IMAGE/default) | netgear-dni | check-size $$$$(IMAGE_SIZE)
+endef
+TARGET_DEVICES += netgear_wnr2000-v3
+
 define Device/netgear_wnr612-v2
   $(Device/netgear_ar7240)
   DEVICE_TITLE := Netgear WNR612v2
