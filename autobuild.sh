1df4f57911b2ae88c686c8ed313089652a7ef2a3
Zheng Qian <sotux82@gmail.com>
2021-07-04 07:37:56 +0800

    build: add a script to build my custom roms
    Signed-off-by: James Qian <sotux82@gmail.com>
    
    Signed-off-by: Zheng Qian <sotux82@gmail.com>
    (cherry picked from commit 5bab44981f978b730c8079b6aecf1f9b977e2f85)

---
+++ autobuild.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CPU_PROCESSORS=`cat /proc/cpuinfo | grep "processor" | wc -l`
+
+echo "This computer has $CPU_PROCESSORS processor(s)."
+
+
+./scripts/env switch ac58u-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch cm520-19.07 && make -j$CPU_PROCESSORS
+
+./scripts/env switch newifi3-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch wr1200js-19.07 && make -j$CPU_PROCESSORS
+
+./scripts/env switch tl-wr841nv10-ath79-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch tl-wr841nv7-ath79-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch tl-wr841nv8-ath79-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr3800-ath79-19.07 && make -j$CPU_PROCESSORS
+
+./scripts/env switch wndr3700v4-ar71xx-19.07 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr4300v1-ar71xx-19.07 && make -j$CPU_PROCESSORS
+
