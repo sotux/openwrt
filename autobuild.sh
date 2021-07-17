e256f501da48e149f1ab13732a4ea6542a41d92d
Zheng Qian <sotux82@gmail.com>
2021-07-17 21:24:51 +0800

    update autobuild script
    Signed-off-by: Zheng Qian <sotux82@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -11,9 +11,9 @@ echo "This computer has $CPU_PROCESSORS processor(s)."
 ./scripts/env switch newifi3-21.02 && make -j$CPU_PROCESSORS
 ./scripts/env switch wr1200js-21.02 && make -j$CPU_PROCESSORS
 
-./scripts/env switch wndr3800-ath79-21.02 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr3800-21.02 && make -j$CPU_PROCESSORS
 
-./scripts/env switch wndr3700v4-ar71xx-21.02 && make -j$CPU_PROCESSORS
-./scripts/env switch wndr4300v1-ar71xx-21.02 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr3700v4-21.02 && make -j$CPU_PROCESSORS
+./scripts/env switch wndr4300v1-21.02 && make -j$CPU_PROCESSORS
 
 ./scripts/env switch x86-21.02 && make -j$CPU_PROCESSORS
